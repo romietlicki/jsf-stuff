864ad6acb7e8044ac5b010273ea7b0dc50a97a54
romietlicki <rodrigo.mietlicki@gmail.com>
2020-02-18 10:15:45 -0300

    adding login functionality using jsf and primefaces messages

---
+++ app-jsf/src/br/com/rodrigo/model/Usuario.java
@@ -0,0 +1,59 @@
+package br.com.rodrigo.model;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class Usuario implements Serializable{
+	
+	private String userName;
+	private String password;
+	private Estudante estudante;
+	
+	public String validaLogin(){
+		String user = this.getUserName();
+		String password = this.getPassword();
+		
+		if(user.equals("rodrigo.mietlicki") && password.equals("rodrigo123")) {
+			estudante = new Estudante();
+			return "inicio?faces-redirect=true";
+		} else {
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuario e/ou senha invalidos", ""));
+			return null;
+		}
+	}
+	
+	public String logout() {
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		estudante = null;
+		return "login?faces-redirect=true";
+	}
+		
+
+	public Usuario() {
+		
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
